4effafdd5aa0e348203ab9b6d2dbfc910b0c6e7b
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-07-04 04:42:47 +0800

    added handle bars

--- js/app.js
+++ js/app.js
@@ -3,13 +3,19 @@ $(function(){
         crossroads.parse(newHash);
     }
 
-    Handlebars.registerHelper("adminStatus", function(Admin){
-       console.log("admin",Admin)
-        if (Admin ==true)        
+    Handlebars.registerHelper("adminStatus", function(phoneNumber){
+       console.log("admin",phoneNumber)
+        if (phoneNumber !=null||phoneNumber=="true")        
            return '<span class="badge badge-danger text-success">Completed</span>';
         else       
            return '<span class="badge badge-success text-warning">In Progress</span>';
     })
+        //route page -  index.html
+        var routeRoot = crossroads.addRoute('', function(){
+            //redirect to home
+            window.location.href = "#home";
+        });
+    
     var routeRoot = crossroads.addRoute('/home', function(){
         $(".navbar-collapse li a[href='#manage']").css('color', 'white');
         $(".navbar-collapse li a[href='#profile']").css('color', 'white');
@@ -76,16 +82,47 @@ $(function(){
         $("#divcontent").html(profileTemplate).hide().fadeIn(1000);
 	});
     var routeRoot = crossroads.addRoute('/manage', function(){
-        //redirect to home
-        $(".navbar-collapse li").removeClass('active');
-
         $(".navbar-collapse li a[href='#manage']").css('color', 'green');
         $(".navbar-collapse li a[href='#profile']").css('color', 'white');
         $(".navbar-collapse li a[href='#home']").css('color', 'white');
         $(".navbar-collapse li a[href='#bookings']").css('color', 'white');
-        var manageTemplate = Handlebars.templates['manage'];
+        //redirect to home
+		
+
+        if(sessionStorage.getItem('Admin')=="true")
+        {
+        var homeTemplate = Handlebars.templates['manage'];
+        var jsonDataArray=[];
+        
+        $.ajax({
+            type: 'GET',
+            url: 'https://jom-tapau-backend.onrender.com/user',
+            dataType:"JSON",
+            data: JSON.stringify(),
+            success: function(data) {
+  
+           
+                 jsonDataArray = [
+                    ...data
+                ];
+               
+                console.log(jsonDataArray);
+                var renderedTemplate = homeTemplate({ "users": jsonDataArray });
+                $('#divcontent').html(renderedTemplate).hide().fadeIn(1000);
+            
+            },
+            error: function() {
+              
+            }
+          });
+        
+         
+       
+        }
+        else 
+        var homeTemplate = Handlebars.templates['manageUser'];
         $("#divcontent").empty();
-        $("#divcontent").html(manageTemplate).hide().fadeIn(1000);
+        $("#divcontent").html(homeTemplate).hide().fadeIn(1000);
 	});
     hasher.initialized.add(parseHash); //parse initial hash
 	hasher.changed.add(parseHash); //parse hash changes
